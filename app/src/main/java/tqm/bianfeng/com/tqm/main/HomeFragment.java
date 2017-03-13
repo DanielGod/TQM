e0fff184099593719914ff296d5bc772bebb8bc7
Daniel <851976722@qq.com>
2017-03-13 13:08:47 +0800

    fragment切换框架（wjd）

---
+++ app/src/main/java/tqm/bianfeng/com/tqm/main/HomeFragment.java
@@ -0,0 +1,36 @@
+package tqm.bianfeng.com.tqm.main;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import tqm.bianfeng.com.tqm.R;
+
+
+public class HomeFragment extends Fragment {
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+
+        return fragment;
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+
+}
