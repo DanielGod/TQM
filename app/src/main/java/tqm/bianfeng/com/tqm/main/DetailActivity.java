79d112b1f6560e62e70c1b9d6734d18d69dbb167
gqf1995 <gqf1995@qq.com>
2017-03-24 15:56:14 +0800

    完成详情页面功能（gqf）

--- app/src/main/java/tqm/bianfeng/com/tqm/main/DetailActivity.java
+++ app/src/main/java/tqm/bianfeng/com/tqm/main/DetailActivity.java
@@ -2,7 +2,6 @@ package tqm.bianfeng.com.tqm.main;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -15,6 +14,8 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Toast;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
 import com.umeng.socialize.ShareAction;
 import com.umeng.socialize.UMShareAPI;
 import com.umeng.socialize.UMShareListener;
@@ -53,8 +54,15 @@ public class DetailActivity extends BaseActivity {
     boolean isCollection = false;
     boolean isInCollection = false;
     public int detailId = -1;
-    @BindView(R.id.fab)
-    FloatingActionButton fab;
+    @BindView(R.id.multiple_actions_down)
+    FloatingActionsMenu multipleActionsDown;
+    @BindView(R.id.action_a)
+    FloatingActionButton actionA;
+    @BindView(R.id.action_b)
+    FloatingActionButton actionB;
+    @BindView(R.id.action_c)
+    FloatingActionButton actionC;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,8 +90,8 @@ public class DetailActivity extends BaseActivity {
         }
         setToolbar(detailToolbar, toolbarTitle);
         initWebView();
+        initactionASrc();
         initCollection();
-        initFabSrc();
     }
 
     String url;
@@ -93,9 +101,9 @@ public class DetailActivity extends BaseActivity {
         settings.setAppCacheEnabled(true);//设置启动缓存
         settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);//缓存模式
         settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);//4.4以下版本自适应页面大小 不能左右滑动
-//        1.NARROW_COLUMNS：可能的话使所有列的宽度不超过屏幕宽度
-//        2.NORMAL：正常显示不做任何渲染
-//        3.SINGLE_COLUMN：把所有内容放大webview等宽的一列中
+        //        1.NARROW_COLUMNS：可能的话使所有列的宽度不超过屏幕宽度
+        //        2.NORMAL：正常显示不做任何渲染
+        //        3.SINGLE_COLUMN：把所有内容放大webview等宽的一列中
         settings.setUseWideViewPort(true);//设置webview推荐使用的窗口
         settings.setLoadWithOverviewMode(true);//设置webview加载的页面的模式
         settings.setTextZoom(100);//字体大小
@@ -111,10 +119,11 @@ public class DetailActivity extends BaseActivity {
         url = "http://211.149.235.17:8080/tqm-web/app/getDetail/" + detailType + "/" + detailId;
         webView.loadUrl(url);
 
+
     }
 
     public void initCollection() {
-        if (realm.where(User.class).findFirst() != null&&!detailType.equals("04") ) {
+        if (realm.where(User.class).findFirst() != null && !detailType.equals("04")) {
             Subscription subscription = NetWork.getUserService().isAttention(detailId, detailType, realm.where(User.class).findFirst().getUserId())
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
@@ -136,7 +145,7 @@ public class DetailActivity extends BaseActivity {
                             } else {
                                 isCollection = true;
                             }
-                            initFabSrc();
+                            initactionASrc();
                         }
                     });
             compositeSubscription.add(subscription);
@@ -145,7 +154,7 @@ public class DetailActivity extends BaseActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-            getMenuInflater().inflate(R.menu.collection_article_false, menu);
+        getMenuInflater().inflate(R.menu.collection_article_false, menu);
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -153,7 +162,7 @@ public class DetailActivity extends BaseActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         //社会化分享
-        if (item.getItemId() == R.id.collection_false){
+        if (item.getItemId() == R.id.collection_false) {
             share();
         }
         return super.onOptionsItemSelected(item);
@@ -169,28 +178,25 @@ public class DetailActivity extends BaseActivity {
         public void onError(Throwable e) {
             isInCollection = false;
             animation.cancel();
-            Toast.makeText(DetailActivity.this,"网络问题，关注失败",Toast.LENGTH_SHORT).show();
+            Toast.makeText(DetailActivity.this, "网络问题，关注失败", Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onNext(ResultCode resultCode) {
-            if(resultCode.getCode()==ResultCode.SECCESS) {
+            if (resultCode.getCode() == ResultCode.SECCESS) {
                 isCollection = !isCollection;
-                initFabSrc();
+                initactionASrc();
                 toastFocuseResult();
-            }else{
-                Toast.makeText(DetailActivity.this,"关注失败，请重试",Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(DetailActivity.this, "关注失败，请重试", Toast.LENGTH_SHORT).show();
             }
             isInCollection = false;
             animation.cancel();
         }
     };
 
-    @OnClick(R.id.fab)
-    public void onClick() {
-        fabFocuse();
-    }
-    public void share(){
+
+    public void share() {
         UMWeb web = new UMWeb(url);
         web.setTitle(detailTitle);//标题
         web.setDescription(detailTitle);
@@ -232,9 +238,11 @@ public class DetailActivity extends BaseActivity {
         UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
 
     }
+
     RotateAnimation animation;
+
     //关注网络接口调用
-    public void fabFocuse(){
+    public void actionAFocuse() {
         //收藏当前文章
         if (realm.where(User.class).findFirst() == null) {
             Toast.makeText(this, "请登录后再关注", Toast.LENGTH_SHORT).show();
@@ -260,22 +268,28 @@ public class DetailActivity extends BaseActivity {
             }
         }
     }
+
     //关注按钮背景变换
-    public void initFabSrc(){
-        if(!detailType.equals("04")) {
+    public void initactionASrc() {
+        if (!detailType.equals("04")) {
             if (isCollection) {
                 //关注状态
-                fab.setImageResource(R.drawable.ic_successful);
+                actionA.setIcon(R.drawable.ic_successful);
+                actionA.setTitle("已关注");
             } else {
                 //未关注状态
-                fab.setImageResource(R.drawable.ic_failure);
+                actionA.setIcon(R.drawable.ic_failure);
+                actionA.setTitle("未关注");
             }
-        }else{
-            fab.setVisibility(View.GONE);
+            multipleActionsDown.setVisibility(View.VISIBLE);
+            actionC.setVisibility(View.GONE);
+        } else {
+            actionC.setVisibility(View.VISIBLE);
+            multipleActionsDown.setVisibility(View.GONE);
         }
     }
 
-    public void toastFocuseResult(){
+    public void toastFocuseResult() {
         if (isCollection) {
             //关注成功
             Toast.makeText(this, "关注成功，请在猫舍查看", Toast.LENGTH_SHORT).show();
@@ -286,7 +300,7 @@ public class DetailActivity extends BaseActivity {
     }
 
     public void refAnim() {
-        if(animation==null) {
+        if (animation == null) {
             animation = new RotateAnimation(0, 720, Animation.RELATIVE_TO_SELF,
                     0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
             AccelerateDecelerateInterpolator lir = new AccelerateDecelerateInterpolator();
@@ -295,8 +309,23 @@ public class DetailActivity extends BaseActivity {
             animation.setRepeatCount(Animation.INFINITE);//设置重复次数
             animation.setFillAfter(true);//动画执行完后是否停留在执行完的状态
         }
-        fab.setImageResource(R.drawable.ic_loding_anim_img);
-        fab.startAnimation(animation);
+        actionA.setIcon(R.drawable.ic_loding_anim_img);
+        actionA.startAnimation(animation);
+    }
+
+    @OnClick({R.id.action_a, R.id.action_b,R.id.action_c})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.action_a:
+                actionAFocuse();
+                break;
+            case R.id.action_b:
+                webView.setScrollY(0);
+                break;
+            case R.id.action_c:
+                webView.setScrollY(0);
+                break;
+        }
     }
 
 }
